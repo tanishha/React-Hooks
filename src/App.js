191582933a0ca478309b2d5adf3be16bc1a00ee9
Tanisha Chaudhary <tanishach203@gmail.com>
2021-06-16 23:55:43 +0545

    useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
 function App() {
+  const [val,setVal]=useState("Tanisha");
+  const changeVal=()=>{
+    setVal('Tanisha Chaudhary')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>{val}</h1>
+       <button className="btn" onClick={changeVal}>Click me</button>
       </header>
     </div>
   );
