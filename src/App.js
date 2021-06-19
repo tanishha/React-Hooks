affd4344c273948cb1d2bd8309322cf7b7c563a8
Tanisha Chaudhary <tanishach203@gmail.com>
2021-06-19 23:54:40 +0545

    counter using useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React,{useState} from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [val,setVal]=useState("Tanisha");
-  const changeVal=()=>{
-    setVal('Tanisha Chaudhary')
-  }
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if (count >= 1) {
+      document.title = `Count (${count})`;
+    } else {
+      document.title = `Count`;
+    }
+  });
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>{val}</h1>
-       <button className="btn" onClick={changeVal}>Click me</button>
-      </header>
+      <div>
+        <h1>{count}</h1>
+        <button className="btn" onClick={() => setCount(count + 1)}>
+          Click⏱️
+        </button>
+      </div>
     </div>
   );
 }
